266ee08f7087f89e99e5892ec3881066bf60a4e9
pablo <pablo@DESKTOP-61B9SO4.localdomain>
2022-12-16 13:30:24 +0100

    v3.- Seleccionamos el numero      Click derecho->Refactor->Introduce->constante      Ponemos la visibilidad public y le damos el nombre especificado

--- src/es/calcularpi/CalcularPI.java
+++ src/es/calcularpi/CalcularPI.java
@@ -7,10 +7,10 @@ import java.math.MathContext;
 public class CalcularPI {
 
      public static void main(String[] args) {
-        MathContext mc = new MathContext(10);         
+        MathContext mc = new MathContext(NUM_DIG);         
         //meter en el método compute
         BigDecimal pi = new BigDecimal(0);
-        BigDecimal limit = new BigDecimal(1).movePointLeft(10);
+        BigDecimal limit = new BigDecimal(1).movePointLeft(NUM_DIG);
         boolean stop = false;
         for (int k = 0; !stop; k++) {
             BigDecimal piK = piFunction(k, mc);
@@ -23,6 +23,12 @@ public class CalcularPI {
         System.out.println(pi.round(mc));
         //meter en el método compute
     }
+    public static final int NUM_DIG = 10; 
+    /*
+     Seleccionamos el numero
+     Click derecho->Refactor->Introduce->constante
+     Ponemos la visibilidad public y le damos el nombre especificado
+     */
 
      
      /*
